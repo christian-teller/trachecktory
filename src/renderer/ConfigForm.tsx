bc3a9854b25eeb2ffc7e85b4fc14b5983a75a464
Christian Teller <christianteller@gmx.com>
2023-03-03 09:54:51 -0500

    headers in progress

--- src/renderer/ConfigForm.tsx
+++ src/renderer/ConfigForm.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 import {
+  Select,
   TextInput,
   Button,
   Space,
@@ -21,7 +22,7 @@ export default function ConfigForm() {
     initialValues: {
       client_id: '',
       client_secret: '',
-      serverUrl: 'https://qa-gimli.ci.apexpickup.com',
+      server_url: '',
       serverEndpoint: '/token',
       requestMethod: '',
       grant_type: 'client_credentials',
@@ -30,13 +31,35 @@ export default function ConfigForm() {
     },
   });
 
+  let keyValuePairs = {
+    key: '',
+    value: '',
+  };
+
+  let customHeaders = `${keyValuePairs.key} : ${keyValuePairs.value}`;
+
+  const handleKeyValueChange = (e: any) => {
+    if (e.target.id === 'new_key') {
+      keyValuePairs.key = e.target.value;
+      console.log(`new key is currently: ${e.target.value}`);
+    }
+    if (e.target.id === 'new_value') {
+      keyValuePairs.value = e.target.value;
+      console.log(`new value is currently: ${e.target.value}`);
+    }
+  };
+
+  function handleAddHeader() {
+    console.log(customHeaders);
+  }
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log(form.values);
     console.log(e);
     axios
       .post(
-        form.values.serverUrl + form.values.serverEndpoint,
+        form.values.server_url + form.values.serverEndpoint,
         qs.stringify({
           client_id: form.values.client_id.toString(),
           client_secret: form.values.client_secret.toString(),
@@ -64,7 +87,7 @@ export default function ConfigForm() {
     <Container className="contain">
       <form className="auth" onSubmit={handleSubmit}>
         <Grid>
-          <Grid.Col className="user-creds" p={20} sm={4}>
+          <Grid.Col className="user-creds" p={20} sm={5}>
             <Title align="center" order={4}>
               Authentication
             </Title>
@@ -84,27 +107,57 @@ export default function ConfigForm() {
                 placeholder="Client Secret"
                 {...form.getInputProps('client_secret')}
               />
+              <Space h="md" />
+              <Select
+                label="URL"
+                data={[
+                  {
+                    value: 'https://qa-gimli.ci.apexpickup.com',
+                    label: 'qa-gimli.ci.apexpickup.com',
+                  },
+                  {
+                    value: 'https://uat-gimli.stg.apexpickup.com',
+                    label: 'uat-gimli.stg.apexpickup.com',
+                  },
+                ]}
+                {...form.getInputProps('server_url')}
+              />
             </Box>
             <Space h="lg" />
             <Button type="submit">SET</Button>
           </Grid.Col>
-          <Grid.Col p={20} sm={8}>
+          <Grid.Col p={20} sm={7}>
             <Title align="center" order={4}>
               Headers
             </Title>
             <Divider size="md" my="xs" labelPosition="center" />
             <Box className="headers">
               <Group grow align="flex-end" position="center">
-                <TextInput mx={0} label="KEY" placeholder="KEY" size="xs" />
-                <TextInput mx={0} label="VALUE" placeholder="VALUE" size="xs" />
+                <TextInput
+                  onChange={handleKeyValueChange}
+                  id="new_key"
+                  name="KEY"
+                  mx={0}
+                  label="KEY"
+                  placeholder="KEY"
+                  size="xs"
+                />
+                <TextInput
+                  onChange={handleKeyValueChange}
+                  id="new_value"
+                  name="VALUE"
+                  mx={0}
+                  label="VALUE"
+                  placeholder="VALUE"
+                  size="xs"
+                />
                 <ActionIcon
                   sx={{
-                    '&[data-disabled]': { opacity: 0.4 },
-                    '&[data-loading]': { backgroundColor: 'red' },
-                    maxWidth: '20px',
+                    maxWidth: '40px',
                   }}
                   size={30}
                   variant="filled"
+                  onClick={handleAddHeader}
                 >
                   +
                 </ActionIcon>
